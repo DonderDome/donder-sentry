affd6a9b6e954ecbcb403fa48fe9e5fa4db6d64e
Muffles <ruimolefas@Ruis-MBP.lan>
2025-01-22 16:40:08 +0000

    WIP

--- src/donder-sentry.ts
+++ src/donder-sentry.ts
@@ -57,7 +57,7 @@ export class BoilerplateCard extends LitElement {
       throw new Error('Invalid configuration');
     }
 
-    // window.location.href = "/5c36e1c9_hakit/ingress";
+    window.location.href = "/local/ha-dashboard/index.html";
 
     if (config.test_gui) {
       getLovelace().setEditMode(true);
